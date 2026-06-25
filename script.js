e87f7a97b600d48ff59b0b438d6ef9e66fceb0f2
sathyrvictim <sathyrvictim@gmail.com>
2026-06-25 20:29:05 +0700

    add constructor

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const myLibrary = [];
 
-function Book() {
-
-
+function Book(name, author, pages, read) {
+this.name = name;
+this.author = author;
+this.pages = pages;
+this.read = read;
 }
 
-function addBookToLibrary(){
-    
+function addBookToLibrary(name, author, pages, read) {
+   
 }
