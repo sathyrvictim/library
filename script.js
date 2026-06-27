0921b2ccbc857164357aa970aae3f403254118b0
sathyrvictim <sathyrvictim@gmail.com>
2026-06-28 00:59:12 +0700

    add some function to learn console.log vs return

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-// const myLibrary = [];
+const myLibrary = [];
 
-function Book(name, author, pages, read) {
-this.name = name;
-this.author = author;
-this.pages = pages;
-this.read = read;
+function Book(name, author, pages, read){
+    if (!new.target){
+        throw Error("You must use the 'new' operator to call the constructor ")
+    }
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return (`${this.name} ${this.author} ${this.pages} ${this.read}`);
+    }
 }
 
-// function addBookToLibrary(name, author, pages, read) {}
+const theHobbit = new Book("The Hobbit", "by J.R.R. Tolkien", "295 pages", "not read yet");
+
+function addBookToLibrary() {
+const newBook = new Book(name, author, pages, read);
+
+}
+
+
+
+
+function Player(name, marker){
+    if (!new.target){
+        throw Error("You must use the 'new' operator to call the constructor ")
+    }
+ this.name = name ;
+ this.marker = marker;
+ this.sayName = function(){
+    console.log(`${this.name} the ${this.marker}`)
+ }
+}
+
+const playerOne = new Player('fajar', 'jobless');
+
+console.log(`${playerOne.name} the ${playerOne.marker}`)
